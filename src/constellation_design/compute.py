96680e6d2511c1148fa9e2632b864f3731a3686d
Yann BLAUDIN DE THE <yann.blaudin-de-the@thalesaleniaspace.com>
2023-06-27 16:54:21 +0200

    Walker generation OK

--- src/constellation_design/compute.py
+++ src/constellation_design/compute.py
@@ -1,7 +1,7 @@
 from pymoo.core.mixed import MixedVariableGA
 from pymoo.optimize import minimize
 from pymoo.core.problem import ElementwiseProblem
-from pymoo.core.variable import Real, Integer, Choice, Binary
+from pymoo.core.variable import Real, Integer
 
 
 # https://pymoo.org/customization/mixed.html
@@ -10,22 +10,18 @@ from pymoo.core.variable import Real, Integer, Choice, Binary
 class MixedVariableProblem(ElementwiseProblem):
     def __init__(self, **kwargs):
         vars = {
-            "b": Binary(),
-            "x": Choice(options=["nothing", "multiply"]),
-            "y": Integer(bounds=(0, 2)),
-            "z": Real(bounds=(0, 5)),
+            "inc": Real(bounds=(0, 180)),
+            "nsat": Integer(bounds=(1, 10000)),
+            "npla": Integer(bounds=(1, 36)),
+            "pha": Real(bounds=(0, "npla")),
+            "alt": Real(bounds=(500, 1500)),
         }
         super().__init__(vars=vars, n_obj=1, **kwargs)
 
     def _evaluate(self, X: dict, out: dict, *args, **kwargs):
-        b, x, z, y = X["b"], X["x"], X["z"], X["y"]
+        inc, nsat, npla, pha, alt = X["inc"], X["nsat"], X["npla"], X["pha"], X["alt"]
 
-        f = z + y
-        if b:
-            f = 100 * f
-
-        if x == "multiply":
-            f = 10 * f
+        f = inc + nsat + npla + pha + alt
 
         out["F"] = f
 
@@ -34,6 +30,6 @@ problem = MixedVariableProblem()
 
 algorithm = MixedVariableGA(pop=10)
 
-res = minimize(problem, algorithm, termination=("n_evals", 1000), seed=1, verbose=False)
+res = minimize(problem, algorithm, termination=("n_evals", 10000), seed=1, verbose=False)
 
 print("Best solution found: \nX = %s\nF = %s" % (res.X, res.F))
